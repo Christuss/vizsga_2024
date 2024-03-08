2cc2576b273e64f1f5a656f89e3ffe031f6827b4
Christuss <szendefi.krisztofer@gmail.com>
2024-03-08 15:44:16 +0100

    axios get

--- vizsga_react_2024/src/DataService.js
+++ vizsga_react_2024/src/DataService.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
-export default axios.create({
+export default class DataService {
+  constructor() {
+  };
 
-    baseURL: "http://localhost:8000",
-
-});
-
-axios.get(url)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+  get(url, callback) {
+    axios.get(url)
+    .then(function (response) {
+      // handle success
+      callback(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    })
+  }
+};
--- vizsga_react_2024/src/components/Lista.js
+++ vizsga_react_2024/src/components/Lista.js
@@ -1,21 +1,39 @@
-import axios from "axios";
+import DataService from "../DataService";
+import { useState } from "react";
 
 function Lista() {
+  const DS = new DataService();
+  DS.get('http://localhost:8000/api/bejegyzesek/1', listaBeallit);
+  const [lista, setLista] = useState([]);
+
+  function listaBeallit(iras) {
+    setLista(iras);
+  }
+
   return (
     <div className="Lista">
       <table className="table">
         <thead>
-        <tr>
-        <th>Osztály</th>
-        <th>Tevékenység</th>
-        <th>Pont</th>
-        <th>Státusz</th>
-        </tr>
+          <tr>
+            <th>Osztály</th>
+            <th>Tevékenység</th>
+            <th>Pont</th>
+            <th>Státusz</th>
+
+          </tr>
         </thead>
         <tbody>
-        
-        </tbody>
+          {lista.map((elem) =>
+            <tr>
+              <td>{elem.osztaly_id}</td>
+              <td>{elem.tevekenyseg_nev}</td>
+              <td>{elem.pontszam}</td>
+              <td>{elem.allapot}</td>
+            </tr>
+            
+          )}
 
+        </tbody>
       </table>
     </div>
   );
